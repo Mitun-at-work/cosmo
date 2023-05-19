967bae2671b3351a83d90749ab90ca518f156f74
Mitun Suresh <mitun.focused@gmail.com>
2023-05-19 21:44:19 +0530

    dependdency update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 class Cosmo:
     def __init__(self):
